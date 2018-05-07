15e3349d5d27f1b8a154c57bf0deab0dbd7e064d
Bobby <gd.triebkraft@gmail.com>
2018-05-08 02:15:37 +0500

    some changes in setup_mcu.c

--- src/setup_mcu.c
+++ src/setup_mcu.c
@@ -3,6 +3,13 @@
 #include "hdr/setup_mcu.h"
 
 void Setup_MCU(void){
+//Настройка портов ввода/вывода
     DDRB = 0xFF;
     PORTB = 0x00;
+
+//Настройка ADC
+    Init_ADC();
+
+//Настройка таймера
+    Init_timer(100);
 }
