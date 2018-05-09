2f08607d7cd364806c3736000478c409678f0a8e
Bobby <gd.triebkraft@gmail.com>
2018-05-09 11:54:48 +0500

    ADC lib completed

--- src/adc_lib.c
+++ src/adc_lib.c
@@ -9,7 +9,8 @@ volatile uint8_t key1;
 volatile uint8_t key2;
 
 void Init_ADC(void){
-    ;
+    ADMUX = (1 << REFS0) | (1 << ADLAR) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0);
+    ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADIF) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1);
 }
 
 uint8_t Get_key1(void) {
@@ -18,4 +19,12 @@ uint8_t Get_key1(void) {
 
 uint8_t Get_key2(void) {
     return key2;
+}
+
+ISR (ADC_vect) {
+    uint8_t reg = ADMUX;
+    if (reg & 1) key2 = ADCH;
+        else key1 = ADCH;
+    ADMUX ^= 1 << MUX0;
+    ADCSRA |= 1 << ADSC;
 }
--- src/setup_mcu.c
+++ src/setup_mcu.c
@@ -1,8 +1,11 @@
 #include <avr/io.h>
 #include <avr/iom8.h>
+#include <avr/interrupt.h>
 #include "hdr/setup_mcu.h"
 
 void Setup_MCU(void){
+//Запрет прерываний на время инициализации
+    cli();
 //Настройка портов ввода/вывода
     DDRB = 0xFF;
     PORTB = 0x00;
@@ -12,4 +15,7 @@ void Setup_MCU(void){
 
 //Настройка таймера
     Init_timer(100);
+
+//Теперь прерывания разрешены
+    sei();
 }
