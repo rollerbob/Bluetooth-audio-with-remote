0e8ce9ddd4ef22cdb05e9f6a99587ab50a0aeeb4
Bobby <gd.triebkraft@gmail.com>
2018-05-10 02:30:59 +0500

    night commit

--- src/timer.c
+++ src/timer.c
@@ -11,4 +11,11 @@ volatile uint8_t frame;
 void Init_timer (uint8_t fps)
 {
     frame = 0;
+    TCCR2 = (1 << WGM21) | (1 << CS22);
+    TIMSK = 1 << OCIE2;
+    OCR2 = 50;
+}
+
+ISR (TIMER2_COMP_vect) {
+    PORTB ^=1;
 }
