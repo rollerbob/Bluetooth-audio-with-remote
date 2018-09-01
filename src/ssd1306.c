1c4ba107d957e46713bc1f96e39abfb62b94be12
rollerbob <gd.triebkraft@gmail.com>
2018-09-01 21:40:20 +0500

    TWI added

--- src/ssd1306.c
+++ src/ssd1306.c
@@ -1,9 +1,25 @@
 #include <avr/io.h>
+#include <util/delay.h>
 #include "ssd1306.h"
 #include "twi_lib.h"
+
+#define DC_BIT						6
+#define CONTROL_BYTE_COMMAND		0 << DC_BIT  
+#define CONTROL_BYTE_DATA			1 << DC_BIT 
+
+// Локальные функции
+void Send_command(uint8_t command);
+void Send_data(uint8_t data);
 
 void Init_lcd(void)
 {
 	Twi_init();
-	twi_send(SSD1306_ADDR, )
+	_delay_ms (500);
+	Send_command(0xAE);
+}
+
+void Send_command (uint8_t command)
+{
+	uint8_t msg[] = {CONTROL_BYTE_COMMAND, command};
+	Twi_send (msg, 2);
 }
--- src/twi_lib.c
+++ src/twi_lib.c
@@ -1,86 +1,52 @@
+//РџР›РђРўР¤РћР РњРћР—РђР’РРЎРРњРђРЇ Р§РђРЎРўР¬
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "twi_lib.h"
 
-// Переменные
-volatile static uint8_t buff[BUFF_SIZE];
-volatile static uint8_t twi_msg_size;
-volatile static uint8_t twi_state = TWI_NO_STATE;
+//РСЃРїРѕР»СЊР·СѓРµРјС‹Рµ РїРµСЂРµРјРµРЅРЅС‹Рµ
+volatile uint8_t *_msg;
+volatile uint8_t _msg_size;
+volatile uint8_t twi_state = TWI_NO_STATE;
 
-
-/**
- * Инициализация модуля TWI
- */
 void Twi_init(void)
 {
-    TWBR = TWBR_VALUE;
-    TWSR = 0;
-    TWCR = 1 << TWEN;
-    twi_msg_size = 0;
+	//РЈСЃС‚Р°РЅРѕРІРєР° СЃРєРѕСЂРѕСЃС‚Рё РїРµСЂРµРґР°С‡Рё
+	TWBR = TWI_SPEED;
+	TWSR = 0;
+	TWCR = 1 << TWEN;
 }
 
-/**
- * Возвращает текущее состояние модуля TWI
- * @return uint8_t  twi_state  состояние модуля TWI
- */
-uint8_t twi_get_state(void)
+void Twi_send(uint8_t *msg, uint8_t msg_size)
 {
-	return twi_state;
-}
-
-void twi_send(uint8_t addr, uint8_t *msg, uint8_t msg_size)
-{
-	//Сохраняю длинну сообщения во внутреннюю переменную
-	twi_msg_size = msg_size + 1;
-
-	//Если нулевой бит первого байта массива установлен в 0, значит сообщение
-	//нужно передать. 
-	if (!(addr & 1)) {
-
-	//Для этого я входной массив копирую во внутренний массив
-		buff[0] = addr;
-		int i;
-		for (i = 0; i < msg_size; i++) buff[i+1] = msg[i];
-
-	//Иначе я копирую адрес только в нулевой элемент внутреннего массива
-	} else buff[0] = addr;
-
-	//Состояние TWI-модуля - занят
+	_msg = msg;
+	_msg_size = msg_size;
+	//РЎРѕСЃС‚РѕСЏРЅРёРµ TWI-РјРѕРґСѓР»СЏ - Р·Р°РЅСЏС‚
 	twi_state = TWI_BUSY;
 
-	//Запускаю передачу!
+	//Р—Р°РїСѓСЃРєР°СЋ РїРµСЂРµРґР°С‡Сѓ!
 	TWCR = 1 << TWEN | 1 << TWIE | 1 << TWINT | 1 << TWSTA;
-}
-
-void twi_get_data(uint8_t *msg, uint8_t msg_size)
-{
-	uint8_t i;
-	for (i = 0; i<msg_size; i++)
-	{
-		msg[i] = buff[i+1];
-	}
 }
 
 ISR (TWI_vect)
 {
 	static uint8_t i;
-	uint8_t stat = TWSR & TWSR_MASK;
+	uint8_t twi_status = TWSR & TWSR_MASK;
 
-	switch (stat)
+	switch (twi_status)
 	{
 		case TWI_START:
 		case TWI_REP_START:
 			i = 0;
-			TWDR = buff[i++];
+			TWDR = LCD_ADDR;
 			TWCR = 1 << TWINT | 1 << TWEN | 1 << TWIE;
 		break;
 
 		case TWI_MTX_ADR_ACK:
 		case TWI_MTX_DATA_ACK:
-			if (i < twi_msg_size)
+			if (i < _msg_size)
 			{
-				TWDR = buff[i];
-				i++;
+				TWDR = _msg[i++];
 				TWCR = 1 << TWINT | 1 << TWEN | 1 << TWIE;
 			} else 
 			{
@@ -90,10 +56,10 @@ ISR (TWI_vect)
 		break;
 
 		case TWI_MRX_DATA_ACK:
-       		buff[i++] = TWDR;
+       		_msg[i++] = TWDR;
     
     	case TWI_MRX_ADR_ACK:
-      		if (i < (twi_msg_size-1)){
+      		if (i < (_msg_size-1)){
         		TWCR = 1 << TWEN | 1 << TWIE | 1 << TWINT | 1 << TWEA;
       		}
       		else {
@@ -102,7 +68,7 @@ ISR (TWI_vect)
       	break; 
       
     	case TWI_MRX_DATA_NACK:
-      		buff[i] = TWDR;
+      		_msg[i] = TWDR;
       		twi_state = TWI_SUCCESS;  
       		TWCR = 1 << TWEN | 1 << TWINT | 1 << TWSTO;
       	break;
@@ -112,8 +78,7 @@ ISR (TWI_vect)
     	case TWI_MTX_DATA_NACK:
     	case TWI_BUS_ERROR:
     	default:     
-      		twi_state = stat;                                                                                    
+      		twi_state = twi_status;                                                                                    
       		TWCR = 1 << TWEN | 0 << TWIE | 0 << TWINT | 0<<TWEA | 0 << TWSTA | 0 << TWSTO | 0<<TWWC;
 	}
-
 }
